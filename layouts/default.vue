df736fa044272bc61ad49e3191480e3a06b5c0ac
Deepu <deepu@techgentsia.com>
2023-05-09 16:38:49 +0530

    basic routes added

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <nav class="flex justify-center mt-5">
+        <NuxtLink class="px-4 py-2 border" to="/">Home</NuxtLink>
+        <!-- <NuxtLink to="/welcome">Welcome</NuxtLink> -->
+    </nav>
+    <main>
+          <slot />
+    </main>
+</div>
+</template>
